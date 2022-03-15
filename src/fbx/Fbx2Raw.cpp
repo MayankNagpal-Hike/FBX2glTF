15e7186a94da1a1e1a1b88ea79331283791963c1
Mayank Nagpal <mayank.n@hike.in>
2022-03-15 16:25:42 +0530

    Fix

--- src/fbx/Fbx2Raw.cpp
+++ src/fbx/Fbx2Raw.cpp
@@ -865,48 +865,39 @@ static void ReadAnimations(RawModel& raw, FbxScene* pScene, const GltfOptions& o
         const FbxQuaternion previousRotation = initialNode ? localTransform.GetQ() : previousTransform.GetQ();
         const FbxVector4 previousScaling = initialNode ? computeLocalScale(pNode, pTime) : computeLocalScale(pNode, previousTime); 
 
-        const bool useTranslation = initialNode || (localTranslation[0] - previousTranslation[0] != diffTranslation[0] || localTranslation[1] - previousTranslation[1] != diffTranslation[1] || localTranslation[2] - previousTranslation[2] != diffTranslation[2] || localTranslation[3] - previousTranslation[3] != diffTranslation[3]);
+        const bool useTranslation = initialNode || (localTranslation[0] - previousTranslation[0] - diffTranslation[0] < 0.005 || localTranslation[1] - previousTranslation[1] - diffTranslation[1] < 0.005 || localTranslation[2] - previousTranslation[2] - diffTranslation[2] < 0.005 || localTranslation[3] - previousTranslation[3] - diffTranslation[3] < 0.005);
         const bool useRotation = initialNode || (localRotation[0] - previousRotation[0] != diffRotation[0] || localRotation[1] - previousRotation[1] != diffRotation[1] || localRotation[2] - previousRotation[2] != diffRotation[2] || localRotation[3] - previousRotation[3] != diffRotation[3]);
         const bool useScaling = initialNode || (localScale[0] - previousScaling[0] != diffScaling[0] || localScale[1] - previousScaling[1] != diffScaling[1] || localScale[2] - previousScaling[2] != diffScaling[2] || localScale[3] - previousScaling[3] != diffScaling[3]);
 
-        if (useTranslation)
+        if (!initialNode)
         {
-          channel.translations.push_back(toVec3f(localTranslation) * scaleFactor);
-          if (!initialNode)
-          {
-            diffTranslation[0] = localTranslation[0] - previousTranslation[0];
-            diffTranslation[1] = localTranslation[1] - previousTranslation[1];
-            diffTranslation[2] = localTranslation[2] - previousTranslation[2];
-            diffTranslation[3] = localTranslation[3] - previousTranslation[3];
-          }
-        }
-        if (useRotation)
-        {
-          channel.rotations.push_back(toQuatf(localRotation));
-          if (!initialNode)
-          {
-            diffRotation[0] = localRotation[0] - previousRotation[0];
-            diffRotation[1] = localRotation[1] - previousRotation[1];
-            diffRotation[2] = localRotation[2] - previousRotation[2];
-            diffRotation[3] = localRotation[3] - previousRotation[3];
-          }        
-        }
-        if (useScaling)
-        {
-          channel.scales.push_back(toVec3f(localScale));
-          if (!initialNode)
-          {
-            diffScaling[0] = localScale[0] - previousScaling[0];
-            diffScaling[1] = localScale[1] - previousScaling[1];
-            diffScaling[2] = localScale[2] - previousScaling[2];
-            diffScaling[3] = localScale[3] - previousScaling[3];
-          }   
-        }
+          diffTranslation[0] = localTranslation[0] - previousTranslation[0];
+          diffTranslation[1] = localTranslation[1] - previousTranslation[1];
+          diffTranslation[2] = localTranslation[2] - previousTranslation[2];
+          diffTranslation[3] = localTranslation[3] - previousTranslation[3];
 
-        initialNode = !initialNode ? firstFrameIndex == lastFrameIndex - 1 : false;
+          diffRotation[0] = localRotation[0] - previousRotation[0];
+          diffRotation[1] = localRotation[1] - previousRotation[1];
+          diffRotation[2] = localRotation[2] - previousRotation[2];
+          diffRotation[3] = localRotation[3] - previousRotation[3];
+
+          diffScaling[0] = localScale[0] - previousScaling[0];
+          diffScaling[1] = localScale[1] - previousScaling[1];
+          diffScaling[2] = localScale[2] - previousScaling[2];
+          diffScaling[3] = localScale[3] - previousScaling[3];
+        }
+        if (useTranslation)
+          channel.translations.push_back(toVec3f(localTranslation) * scaleFactor);
+        if (useRotation)
+          channel.rotations.push_back(toQuatf(localRotation));      
+        if (useScaling)
+          channel.scales.push_back(toVec3f(localScale));
+
+        initialNode = !initialNode ? frameIndex == lastFrameIndex - 1 : false;
+        
         if (verboseOutput) {
           fmt::printf("Frame Number %d\n", frameIndex);
-          if (firstFrameIndex == lastFrameIndex - 1)
+          if (frameIndex == lastFrameIndex - 1)
             fmt::printf("Next Last Frame\n");
         }
 
